0f6be221135681670d6a1a30be201fc79ea12b5c
GunDumMan <55555topkung@gmail.com>
2022-05-17 14:06:37 +0700

    fac

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-let netIncome = 999999;
-let tax;
+let n = 6;
+let sum1 = 1;
+let sum = 1;
+let x = 1;
 
-if (netIncome <= 150000) {
-    tax = 0;
-} else if (netIncome <= 300000) {
-    tax = ((netIncome - 150000) * 5 / 100);
-} else if (netIncome <= 500000) {
-    tax = ((netIncome - 300000) * 10 / 100) + 7500;
-} else if (netIncome <= 750000) {
-    tax = ((netIncome - 500000) * 15 / 100) + 27500;
-} else if (netIncome <= 1000000) {
-    tax = ((netIncome - 750000) * 20 / 100) + 65000;
-} else if (netIncome <= 2000000) {
-    tax = ((netIncome - 1000000) * 25 / 100) + 115000;
-} else if (netIncome <= 5000000) {
-    tax = ((netIncome - 2000000) * 30 / 100) + 365000;
-} else {
-    tax = ((netIncome - 5000000) * 35 / 100) + 1265000;
+while (x <= n) {
+    if (n == 0) {
+        sum = 1;
+        break;
+    }
+    sum1 = sum1 * x;
+    x++;
 }
 
-console.log('You have to pay ' + tax);
+console.log(sum1);
+
+for (let i = 1; i <= n; i++) {
+    if (n == 0) {
+        sum = 1;
+        break;
+    }
+    sum = sum * i;
+}
+
+console.log(sum);
