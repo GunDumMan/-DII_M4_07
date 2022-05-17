7bc6c55ecd5a6451db02e31955733953337e75bc
GunDumMan <55555topkung@gmail.com>
2022-05-17 13:51:14 +0700

    tax

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-let score = 0;
-let grade;
-if (score >= 80) {
-    grade = 'A';
-} else if (score >= 75) {
-    grade = 'B+';
-} else if (score >= 70) {
-    grade = 'B';
-}
-else if (score >= 65) {
-    grade = 'C+';
-} else if (score >= 50) {
-    grade = 'C';
-} else {
-    grade = 'F';
-}
-console.log(grade + ' ' + score)
+let netIncome = 999999;
+let tax;
 
+if (netIncome <= 150000) {
+    tax = 0;
+} else if (netIncome <= 300000) {
+    tax = ((netIncome - 150000) * 5 / 100);
+} else if (netIncome <= 500000) {
+    tax = ((netIncome - 300000) * 10 / 100) + 7500;
+} else if (netIncome <= 750000) {
+    tax = ((netIncome - 500000) * 15 / 100) + 27500;
+} else if (netIncome <= 1000000) {
+    tax = ((netIncome - 750000) * 20 / 100) + 65000;
+} else if (netIncome <= 2000000) {
+    tax = ((netIncome - 1000000) * 25 / 100) + 115000;
+} else if (netIncome <= 5000000) {
+    tax = ((netIncome - 2000000) * 30 / 100) + 365000;
+} else {
+    tax = ((netIncome - 5000000) * 35 / 100) + 1265000;
+}
+
+console.log('You have to pay ' + tax);
