88281aadd0b31c86b6a2af1f178d0ad8499e968c
GunDumMan <55555topkung@gmail.com>
2022-05-17 14:16:51 +0700

    fibonacci

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-let n = 6;
-let sum1 = 1;
-let sum = 1;
-let x = 1;
+let n = 10;
+let first = 0;
+let second = 1;
+let first1 = 0;
+let second1 = 1;
+let sum_while = 0;
+let sum_for = 0;
+let count = 1;
 
-while (x <= n) {
-    if (n == 0) {
-        sum = 1;
-        break;
-    }
-    sum1 = sum1 * x;
-    x++;
+while (count < n - 1) {
+    sum_while = first1 + second1;
+    first1 = second1;
+    second1 = sum_while;
+    count++;
 }
 
-console.log(sum1);
+console.log(sum_while);
 
-for (let i = 1; i <= n; i++) {
-    if (n == 0) {
-        sum = 1;
-        break;
-    }
-    sum = sum * i;
+
+for (let i = 1; i < n - 1; i++) {
+    sum_for = first + second;
+    first = second;
+    second = sum_for;
 }
 
-console.log(sum);
+console.log(sum_for);
+
+console.log(sum_while == sum_for);
