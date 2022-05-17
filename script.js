313842be166472b15577205dd7abf3709ba1d2e0
GunDumMan <55555topkung@gmail.com>
2022-05-17 14:38:54 +0700

    fibonacci

--- script.js
+++ script.js
@@ -7,21 +7,35 @@ let sum_while = 0;
 let sum_for = 0;
 let count = 1;
 
-while (count < n - 1) {
-    sum_while = first1 + second1;
-    first1 = second1;
-    second1 = sum_while;
-    count++;
+
+if (n < 3) {
+    sum_while = 1;
+} else {
+    while (count < n - 1) {
+        sum_while = first1 + second1;
+        first1 = second1;
+        second1 = sum_while;
+        count++;
+    }
 }
 
 console.log(sum_while);
 
 
-for (let i = 1; i < n - 1; i++) {
-    sum_for = first + second;
-    first = second;
-    second = sum_for;
+if (n < 3) {
+    sum_for = 1;
+} else {
+    for (let i = 1; i < n - 1; i++) {
+        if (n == 2) {
+            sum_for = 1;
+            break;
+        }
+        sum_for = first + second;
+        first = second;
+        second = sum_for;
+    }
 }
+
 
 console.log(sum_for);
 
