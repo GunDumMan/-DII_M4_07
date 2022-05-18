e954c507e01a436962f51ce7994eb7a25c0b2ed3
GunDumMan <55555topkung@gmail.com>
2022-05-18 11:05:48 +0700

    Quick exercise array

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
-let place = [3];
+let employee = [5];
+let totalSalary = 0;
+let averrageAge = 0;
 
-place[0] = {
-    'name': 'chiang mai',
-    'lat': 19.8046735,
-    'long': 98.9528446,
+employee[0] = {
+    'name': 'นาย ก.',
+    'age': 68,
+    'salary': 75900,
+}
+employee[1] = {
+    'name': 'นาย ข.',
+    'age': 35,
+    'salary': 55400,
+}
+employee[2] = {
+    'name': 'นาย ค.',
+    'age': 51,
+    'salary': 81000,
+}
+employee[3] = {
+    'name': 'นาย ง.',
+    'age': 76,
+    'salary': 111000,
+}
+employee[4] = {
+    'name': 'นาย จ.',
+    'age': 22,
+    'salary': 20500,
 }
 
-place[1] = {
-    'name': 'maya',
-    'lat': 18.8046735,
-    'long': 98.4444,
+for (let i = 0; i < employee.length; i++) {
+    totalSalary = totalSalary + employee[i].salary;
 }
 
-place[2] = {
-    'name': 'central chaing mai',
-    'lat': 18.84,
-    'long': 98.917,
+for (let i = 0; i < employee.length; i++) {
+    averrageAge = averrageAge + employee[i].age;
 }
 
-console.log(place[0].name);
+averrageAge = averrageAge / employee.length;
+
+
+
+console.log('Total salary = ' + totalSalary);
+console.log('Average Age = ' + averrageAge);
