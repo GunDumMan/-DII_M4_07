ea4376bb450efa734886b0e808a0a23bf632238b
GunDumMan <55555topkung@gmail.com>
2022-05-18 12:29:05 +0700

    js-html-showdate

--- script.js
+++ script.js
@@ -7,5 +7,14 @@ function alert2() {
 }
 let ok = document.getElementById('ok');
 let cancel = document.getElementById('cancel');
+let showDate = document.getElementById('showDate');
+
 ok.addEventListener('click', alert1);
 cancel.addEventListener('click', alert2);
+showDate.addEventListener('click', function (e) {
+    document.getElementById('emptySpace').innerHTML = Date();
+});
+
+showDate.addEventListener('dblclick', function (e) {
+    document.getElementById('emptySpace').innerHTML = '';
+});
